9a4fe4ec41f4c25d2fe8dbb638c30d3fd4d0f73f
Patrick Davidson <padavidson@lbl.gov>
2018-07-30 17:22:38 -0700

    remove png, url, jalview export options.

--- src/menu/views/ExportMenu.js
+++ src/menu/views/ExportMenu.js
@@ -18,36 +18,36 @@ const ExportMenu = MenuBuilder.extend({
   render: function() {
     this.setName("Export");
 
-    this.addNode("Share view (URL)" + ShareSym, () => {
-      return Exporter.shareLink(this.msa, function(link) {
-        return window.open(link, '_blank');
-      });
-    });
-
-    this.addNode("View in Jalview", () => {
-      var url = this.g.config.get('url');
-      if (!(typeof url !== "undefined" && url !== null)) {
-        return alert("Sequence weren't imported via an URL");
-      } else {
-        if (url.indexOf("localhost" || url === "dragimport")) {
-          return Exporter.publishWeb(this.msa, (link) => {
-            return Exporter.openInJalview(link, this.g.colorscheme.get("scheme"));
-          });
-        } else {
-          return Exporter.openInJalview(url, this.g.colorscheme.get("scheme"));
-        }
-      }
-    });
+    // this.addNode("Share view (URL)" + ShareSym, () => {
+    //   return Exporter.shareLink(this.msa, function(link) {
+    //     return window.open(link, '_blank');
+    //   });
+    // });
+    //
+    // this.addNode("View in Jalview", () => {
+    //   var url = this.g.config.get('url');
+    //   if (!(typeof url !== "undefined" && url !== null)) {
+    //     return alert("Sequence weren't imported via an URL");
+    //   } else {
+    //     if (url.indexOf("localhost" || url === "dragimport")) {
+    //       return Exporter.publishWeb(this.msa, (link) => {
+    //         return Exporter.openInJalview(link, this.g.colorscheme.get("scheme"));
+    //       });
+    //     } else {
+    //       return Exporter.openInJalview(url, this.g.colorscheme.get("scheme"));
+    //     }
+    //   }
+    // });
 
     this.addNode("Export alignment (FASTA)", () => {
       return Exporter.saveAsFile(this.msa, "all.fasta");
     });
 
-    this.addNode("Export alignment (URL)", () => {
-      return Exporter.publishWeb(this.msa, function(link) {
-        return window.open(link, '_blank');
-      });
-    });
+    // this.addNode("Export alignment (URL)", () => {
+    //   return Exporter.publishWeb(this.msa, function(link) {
+    //     return window.open(link, '_blank');
+    //   });
+    // });
 
     this.addNode("Export selected sequences (FASTA)", () => {
       return Exporter.saveSelection(this.msa, "selection.fasta");
@@ -57,9 +57,9 @@ const ExportMenu = MenuBuilder.extend({
       return Exporter.saveAnnots(this.msa, "features.gff3");
     });
 
-    this.addNode("Export MSA image (PNG)", () => {
-      return Exporter.saveAsImg(this.msa, "biojs-msa.png");
-    });
+    // this.addNode("Export MSA image (PNG)", () => {
+    //   return Exporter.saveAsImg(this.msa, "biojs-msa.png");
+    // });
 
     this.el.appendChild(this.buildDOM());
     return this;
